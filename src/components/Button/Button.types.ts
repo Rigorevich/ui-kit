98e1ac360acb25931c2e7b891181947a59441e11
Rigorevich <rigorevich268@gmail.com>
2023-11-24 13:25:24 +0300

    refactor: Button types

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -1,10 +1,14 @@
-import { MouseEventHandler } from "react";
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 
-export type ButtonTypes = "primary" | "secondary" | "error";
+export type ButtonVariant = 'filled' | 'outlined' | 'transparent';
+export type ButtonColor = 'primary' | 'secondary' | 'success' | 'warning' | 'error' | 'info';
+export type ButtonSize = 'small' | 'medium' | 'large';
 
-export interface ButtonProps {
-  text: string;
-  buttonType?: ButtonTypes;
-  disabled?: boolean;
-  onClick?: MouseEventHandler<HTMLButtonElement>;
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: ButtonVariant;
+  color?: ButtonColor;
+  size?: ButtonSize;
+  startSection?: ReactNode;
+  endSection?: ReactNode;
+  href?: string;
 }
